7fbad3890d6f058776951d771bbfedeec8f885c8
Warat Nikomkaay <warat.ni@ku.th>
2026-09-13 17:51:43 +0700

    [dev] protect endpoints

---
+++ TodoApi/Dtos/LoginDto.cs
@@ -0,0 +1,12 @@
+namespace TodoApi.Dtos
+{
+    public record LoginDto(
+    string Username, 
+    string Password
+);
+
+    public record LoginResponseDto(
+        string Token, 
+        DateTime Expiration
+);
+}
